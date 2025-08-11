7a89389606881c2850020e544eeb710c04003af5
saypaul <saypaul@redhat.com>
2025-08-11 16:24:05 +0530

    License: updated and added to each code files

--- src/lib/greenboot.rs
+++ src/lib/greenboot.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 use anyhow::{Result, bail};
 use glob::glob;
 use std::collections::HashSet;
--- src/lib/handler.rs
+++ src/lib/handler.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 use anyhow::{Context, Result, anyhow, bail};
 use serde_json::Value;
 use std::path::Path;
